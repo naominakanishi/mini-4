6882e8893becc0d52b24dc08e88a0d62ed2850fd
André Arns <andreluisarns@gmail.com>
2022-05-20 15:54:32 -0300

    Fix navigation view

--- AcademyiOS/Scenes/Profile/Views/ProfileView.swift
+++ AcademyiOS/Scenes/Profile/Views/ProfileView.swift
@@ -14,102 +14,96 @@ struct ProfileView: View {
     @State var firstResponder: Int = 0
     
     var body: some View {
-        
-        NavigationView {
-            ScrollView {
-                VStack(alignment: .center, spacing: 30){
-                    
-                    
-                    ProfilePictureView(viewModel: picViewModel)
-                    
-                    CustomTextField(text: .constant("@hannapcf"), //TODO: Input de dados
-                                    firstResponder: .constant(1),
-                                    order: 1)
-                    .modifier(TextFieldModifier())
-                    
-                    VStack(alignment: .leading, spacing: 0){
-                        HStack{
-                            Image(systemName: "hands.sparkles.fill")
-                                .foregroundColor(.adaLightBlue)
-                            Text("Posso ajudar em")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .font(.system(size: 14))
-                            Spacer()
-                            HStack(alignment: .center, spacing: 9){
-                                //TODO: HelpCards aqui, ver com o André
-                            }
-                        }
-                        .padding()
-                    }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white.opacity(0.1).adaGradient(repeatCount: 3))
-                    .cornerRadius(12)
-                    
-                    VStack{
-                        HStack{
-                            Image(systemName: "person.crop.square.filled.and.at.rectangle.fill")
-                                .foregroundColor(.adaLightBlue)
-                            Text("Seu cargo")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .font(.system(size: 14))
-                            Spacer()
-                            HStack(alignment: .center, spacing: 9){
-                                //TODO: RolesCards aqui, ver com o André
-                            }
-                        }
-                        .padding()
-                    }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white.opacity(0.1).adaGradient(repeatCount: 3))
-                    .cornerRadius(12)
-                    
-                    GeneralButton(title: "Disponibilidade",
-                                  image: "calendar.badge.clock",
-                                  enabled: true,
-                                  borderColor: .clear,
-                                  hasOptions: false,
-                                  hasChevron: true,
-                                  hasSavedData: true,
-                                  savedData: "",
-                                  onTap: {})
-                    
-                    GeneralButton(title: "Aniversário",
-                                  image: "calendar",
-                                  enabled: true,
-                                  borderColor: .clear,
-                                  hasOptions: true,
-                                  hasChevron: false,
-                                  hasSavedData: true,
-                                  savedData: "01/01/2000",
-                                  onTap: {})
-                    Spacer()
-                    
-                    Button(action: {
-                        //TODO: Button Action
-                    }, label: {
-                        Text("Salvar")
-                            .padding(14)
+        ScrollView {
+            VStack(alignment: .center, spacing: 30){
+                ProfilePictureView(viewModel: picViewModel)
+                
+                CustomTextField(text: .constant("@hannapcf"), //TODO: Input de dados
+                                firstResponder: .constant(1),
+                                order: 1)
+                .modifier(TextFieldModifier())
+                
+                VStack(alignment: .leading, spacing: 0){
+                    HStack{
+                        Image(systemName: "hands.sparkles.fill")
+                            .foregroundColor(.adaLightBlue)
+                        Text("Posso ajudar em")
                             .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .background(Color.adaLightBlue)
-                            .cornerRadius(12)
-                    })
-                    .padding(.bottom, 51)
-                    
+                            .fontWeight(.bold)
+                            .font(.system(size: 14))
+                        Spacer()
+                        HStack(alignment: .center, spacing: 9){
+                            //TODO: HelpCards aqui, ver com o André
+                        }
+                    }
+                    .padding()
                 }
-                .padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.adaBackground)
-                .edgesIgnoringSafeArea(.all)
+                .frame(maxWidth: .infinity)
+                .background(Color.white.opacity(0.1))
+                .cornerRadius(12)
+                
+                VStack{
+                    HStack{
+                        Image(systemName: "person.crop.square.filled.and.at.rectangle.fill")
+                            .foregroundColor(.adaLightBlue)
+                        Text("Seu cargo")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 14))
+                        Spacer()
+                        HStack(alignment: .center, spacing: 9){
+                            //TODO: RolesCards aqui, ver com o André
+                        }
+                    }
+                    .padding()
+                }
+                .frame(maxWidth: .infinity)
+                .background(Color.white.opacity(0.1))
+                .cornerRadius(12)
+                
+                GeneralButton(title: "Disponibilidade",
+                              image: "calendar.badge.clock",
+                              enabled: true,
+                              borderColor: .clear,
+                              hasOptions: false,
+                              hasChevron: true,
+                              hasSavedData: true,
+                              savedData: "",
+                              onTap: {})
+                
+                GeneralButton(title: "Aniversário",
+                              image: "calendar",
+                              enabled: true,
+                              borderColor: .clear,
+                              hasOptions: true,
+                              hasChevron: false,
+                              hasSavedData: true,
+                              savedData: "01/01/2000",
+                              onTap: {})
+                Spacer()
+                
+                Button(action: {
+                    //TODO: Button Action
+                }, label: {
+                    Text("Salvar")
+                        .padding(14)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.adaLightBlue)
+                        .cornerRadius(12)
+                })
+                .padding(.bottom, 51)
+                
             }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.adaBackground)
+            .edgesIgnoringSafeArea(.all)
         }
         .background(Color.adaBackground)
         .navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
         .navigationTitle("Perfil")
-        .edgesIgnoringSafeArea(.all)
         .sheet(isPresented: $picViewModel.openCameraRoll){
             ImagePicker(selectedImage: $picViewModel.imageSelected,
                         sourceType: .photoLibrary)
